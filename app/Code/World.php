e95627640452b8acf157ab34da0e462b449815d9
Areg Sarkissian <aregsar@gmail.com>
2022-12-08 15:06:18 -0800

    wip

---
+++ app/Code/World.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Code;
+
+class World
+{
+    public function Hello(): void
+    {
+        echo "Hello World";
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,14 +1,18 @@
 <?php
 
 declare(strict_types=1);
-require_once __DIR__ . "/vendor/autoload.php";
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-#error_reporting(E_ALL);
-//add bootstrap code here
+require_once __DIR__ . "/../vendor/autoload.php";
+
+// ini_set('display_errors', '1');
+// ini_set('display_startup_errors', '1');
+// error_reporting(E_ALL);
+
+use App\Code\World;
 
 try {
+    $world = new World();
+    $world->Hello();
 } catch (Exception $e) {
     echo $e->getMessage();
 }
